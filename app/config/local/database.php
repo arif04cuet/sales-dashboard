e87509a14662f11d44411ac4006a9498ac6f1465
shaaheed <msipersonal.bd@gmail.com>
2015-01-17 00:36:16 +0600

    migrations

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
         'mysql' => array(
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => 'scotchbox',
+            'database' => 'sales-dashboard',
             'username' => 'root',
-            'password' => 'root',
+            'password' => '',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('scotchbox'),
+	'local' => array('Shahid-PC'),
 
 ));
 
