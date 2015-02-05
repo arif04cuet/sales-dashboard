8b00712b25e3913d335472efa50aa5ca7aeac5c2
shaaheed <msipersonal.bd@gmail.com>
2015-02-05 14:37:46 +0600

    Order assign to writer

---
+++ app/views/emails/assign-order-writer-qc-blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<div style="margin-left: auto; margin-right: auto; padding-left: 15px; padding-right: 15px; margin-top: 10px;">
+    <div style="text-align: center;">
+        <p>A new assignment has been assigned to you. Please confirm it as soon as possible.</p>
+        <br/>
+        <a href="#"
+           style="border-radius: 2px; font-size: 13px; height: 38px; margin-bottom: 0; background-color: #5cb85c; border-color: #4cae4c; color: #fff; padding:10px 40px; border: none; text-decoration: none;">
+            Accept
+        </a>
+        <a href="#"
+           style="border-radius: 2px; font-size: 13px; height: 38px; margin-bottom: 0; background-color: #d9534f; border-color: #d43f3a; color: #fff; padding:10px 40px; border: none; text-decoration: none;">
+            Decline
+        </a>
+    </div>
+</div>
+</body>
+</html>
